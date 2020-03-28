1ad47ac072ed2d0d85efbf55f7880700c82b8ab7
RafaelReis <rafaelisaiasreis@gmail.com>
2020-03-28 11:25:24 -0300

    Front-End do app

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+
+import './global.css'
+
+//JSX  (JavaScript XML)
+//Um componente no React é uma função que retorna um HTML
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes />
   );
 }
 
